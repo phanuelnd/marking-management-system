abdfcb09cf9e3d40d186a45788f22a39fdf26f02
ISHIMWE Valentin <ishimwevalentin3@gmail.com>
2021-09-22 13:17:58 +0200

    Fixed query exception in appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,11 @@ namespace App\Providers;
 
 use App\Models\Student;
 use App\Models\Teacher;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -30,7 +32,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        View::share('student_confirm_count', Student::unconfirmed()->count());
-        View::share('teacher_confirm_count', Teacher::unconfirmed()->count());
+        try {
+            View::share('student_confirm_count', Student::unconfirmed()->count());
+            View::share('teacher_confirm_count', Teacher::unconfirmed()->count());
+        } catch (QueryException $e) {
+            // dd($e);
+        }
     }
 }
