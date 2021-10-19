dfc10421a8ffdb4e473cb2a973c985037450462a
ISHIMWE <ishimwevalentin3@gmail.com>
2021-10-19 16:34:46 +0200

    added new build

---
+++ public/html/precache-manifest.685fb3c5d01530a53adba0e2ea730ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ae3122c7dcb5ea455fef50fb933645",
+    "url": "/html/index.html"
+  },
+  {
+    "revision": "9542e8910faa001e5e77",
+    "url": "/html/static/css/main.e223c959.chunk.css"
+  },
+  {
+    "revision": "bffade42b5a80030c360",
+    "url": "/html/static/js/2.67ffcac8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/html/static/js/2.67ffcac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9542e8910faa001e5e77",
+    "url": "/html/static/js/main.8b83a6e2.chunk.js"
+  },
+  {
+    "revision": "9e6e9a52cbd1ab382c38",
+    "url": "/html/static/js/runtime-main.92225f40.js"
+  }
+]);
