b389c628cfd36b0f3fc585b20c91e01572ade5e9
ISHIMWE <ishimwevalentin3@gmail.com>
2021-10-19 14:55:58 +0200

    added react client

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,6 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Cors::class,//cors added here
         ],
     ];
 
---
+++ public/precache-manifest.aef335903cb676ca60f9fbb7a257ea6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4fb0fd981c9962dbbf504d223f1123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9542e8910faa001e5e77",
+    "url": "/static/css/main.e223c959.chunk.css"
+  },
+  {
+    "revision": "bffade42b5a80030c360",
+    "url": "/static/js/2.67ffcac8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.67ffcac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9542e8910faa001e5e77",
+    "url": "/static/js/main.8b83a6e2.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  }
+]);
