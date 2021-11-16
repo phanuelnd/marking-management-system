6e0cc9fdeb92ee7a86f4c0306806cfab92ff2518
ISHIMWE <ishimwevalentin3@gmail.com>
2021-11-16 15:11:01 +0200

    fixing cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,10 +16,9 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        // header('Access-Control-Allow-Origin:  *');
-        header('Access-Control-Allow-Origin:  http://localhost:3000');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  POST, PUT');
+        header('Access-Control-Allow-Origin:  *');
+        header('Access-Control-Allow-Headers:  *');
+        header('Access-Control-Allow-Methods:  *');
 
         return $next($request);
     }
